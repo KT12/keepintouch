02e1bcf1daed3c063fc8a66daf62c977fd5d173f
Ken Tsuji <nyy2008@gmail.com>
2017-06-24 14:23:55 -0400

    Pulls in data from LinkedIN
    Places data into pandas DF

--- accesslinkedin.py
+++ accesslinkedin.py
@@ -4,6 +4,8 @@ Spyder Editor
 
 This is a temporary script file.
 """
+# Credentials stored in .env file in same folder
+
 
 import linkedin
 import requests
@@ -11,6 +13,12 @@ import oauth2 as oauth
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 import os
+from xml.etree import ElementTree
+import xmltodict
+import json
+from pandas.io.json import json_normalize
+
+print('Rember to use the correct directory for your machine')
 
 os.chdir('repos/keepint')
 # Load LinkedIn credentials
@@ -32,4 +40,15 @@ URL = 'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_co
 
 # Token originally from LinkedIn JSON
 TOKEN = os.environ.get('ACCESS_TOKEN')
+
+XML_URL = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,picture-url,industry,summary,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)?oauth2_access_token=" + TOKEN
+
+XML_response = requests.get(XML_URL)
+tree = ElementTree.fromstring(XML_response.content)
+
+XML_data = xmltodict.parse(XML_response.content)
+
+person_df = json_normalize(XML_data).T # Transpose dataframe
+
+idx = person_df.index.values
 
